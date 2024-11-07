3f9a186985d1d8dcb866a6ce707e9f18c1b54065
Igor Padoim <igorpadoim@gmail.com>
2024-11-07 13:34:51 -0300

    Fix documentation missing punctuation

--- lib/radventure.rb
+++ lib/radventure.rb
@@ -6,14 +6,14 @@ require_relative 'radventure/meta'
 module Radventure
   # Main class to run the game
   class Game
-    # Constructor method
+    # Constructor method.
     # @returns [Game] Game execution
     def initialize
       $stdout.clear_screen
       intro
     end
 
-    # Outputs a simple intro to the game
+    # Outputs a simple intro to the game.
     # @return [void]
     def intro
       puts "< #{Radventure::TITLE} >"
