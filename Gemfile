534d9820040b2c196fcc736d02aa22ab66c8172d
Igor Padoim <igorpadoim@gmail.com>
2024-11-07 01:29:17 -0300

    Begin documentation

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source 'https://rubygems.org'
 gem 'rake', '~> 13.0'
 gem 'rbs', '~> 3.6'
 gem 'rubocop', '~> 1.21'
+
+gem 'yard', '~> 0.9', group: :development
--- lib/radventure.rb
+++ lib/radventure.rb
@@ -6,17 +6,30 @@ require_relative 'radventure/meta'
 module Radventure
   # Main class to run the game
   class Game
+    # Constructor method
+    # @returns [Game] Game execution
     def initialize
       $stdout.clear_screen
       intro
     end
 
+    # Outputs a simple intro to the game
+    # @return [void]
     def intro
       puts "< #{Radventure::TITLE} >"
       puts "Version #{Radventure::VERSION}"
       puts "By #{Radventure::AUTHOR}"
       puts ''
       puts Radventure::DESCRIPTION
+      wait_for_key
+    end
+
+    # Prints a "press any key" message and waits for, well, any key.
+    # @return [void]
+    def wait_for_key
+      puts ''
+      puts '<Press any key>'
+      gets
     end
   end
 end
