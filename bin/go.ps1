4eea4b598392138d65d2a312c5bd7689e0c276d7
Igor Padoim <igorpadoim@gmail.com>
2024-11-07 12:32:39 -0300

    Add windows execution script

---
+++ bin/go.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+	Install dependencies and run console
+.DESCRIPTION
+	This PowerShell script starts by installing any missing dependencies through Bundler, then it invokes the `console` script through Ruby.
+.LINK
+	https://github.com/Nereare/radventure
+.NOTES
+	Author: Igor Padoim | License: Unlicense
+#>
+Write-Host 'Installing dependencies...'
+bundle install
+
+Write-Host 'Now, run application...'
+try {
+  ruby .\console
+  exit 0
+} catch {
+  Write-Error '⚠️ Could not invoke ruby.'
+  exit 1
+}
